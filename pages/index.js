a50ad46ab5059c7a9ffcac2d7f080cae4ceba54e
Roel <roel.van.bilzen@gmail.com>
2020-05-31 09:48:14 +0200

    implementation beginnings of Material UI

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,12 @@ import useSecurity from '../useSecurity'
 import axios from 'axios'
 import { useState, useEffect } from 'react'
 
+// import React from 'react'
+// import Paper from '@material-ui/core/Paper'
+// import Typography from '@material-ui/core/Typography'
+// import Grid from '@material-ui/core/Grid'
+// import CircularProgress from '@material-ui/core/CircularProgress';
+
 export default function Home() {
   const { isLoggedIn } = useSecurity()
   const [menu, setMenu] = useState([])
@@ -40,7 +46,31 @@ export default function Home() {
       </main>
       
       <footer>
-     
+      {/* <CircularProgress color="secondary" />
+      <Paper >
+            {
+              <img
+                style={{ width: '350px' }}
+                src="https://source.unsplash.com/user/erondu"
+                alt="background"
+              />
+            }
+            <div  />
+            <Grid container>
+              <Grid item md={6}>
+                <div>
+                  <Typography component="h1" variant="h3" color="inherit" gutterBottom>
+                    Title of a longer featured blog post
+                  </Typography>
+                  <Typography variant="h5" color="inherit" paragraph>
+                    Multiple lines of text that form the lede, informing new readers
+                    quickly and efficiently about what&apos;s most interesting in this
+                    post&apos;s contents.
+                  </Typography>
+                </div>
+              </Grid>
+            </Grid>
+          </Paper> */}
       </footer>
     </div>
   )
