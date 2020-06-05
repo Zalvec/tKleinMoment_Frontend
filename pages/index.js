3051572d41f8954bd55df0ec92171a896b4dac0c
Roel <roel.van.bilzen@gmail.com>
2020-06-05 10:07:28 +0200

    Index logic moved to homepagecomponent

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,12 @@
-import useSecurity from '../useSecurity'
 import Layout from '../components/layout'
+import Homepage from '../components/Homepage'
 
 
 export default function Home({footerData}) {
-  const { isLoggedIn } = useSecurity()
 
   return (
     <Layout footerData={footerData} title={`'t Klein Moment`}>
-      <div className="container">
-
-        <main>
-          <h1>Start of 't Klein Moment</h1>
-          {/* TODO: Carousel of images */}
-          {/* TODO: Adding FB & instagram logo-link */}
-          {isLoggedIn && <p>INGELOGD</p> || <p>BEZOEKER</p>}
-        </main>
-        
-      </div>
+      <Homepage footerData={footerData}/>
     </Layout>
   )
 }
