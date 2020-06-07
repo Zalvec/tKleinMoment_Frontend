0af5e5841fd89c4da1ce558761f6235b07b668af
Roel <roel.van.bilzen@gmail.com>
2020-06-07 12:49:45 +0200

    Styling and responsive navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,27 +1,65 @@
+import { useState } from 'react'
 import Link from 'next/link'
+import clsx from 'clsx'
 
 import useSecurity from '../useSecurity'
+import useWindowSize from '../useWindowSize'
 
 export default () => {
+    const size = useWindowSize();
+  
     const { isLoggedIn } = useSecurity()
+    const [ menuStatus, setMenuStatus ] = useState(false)
+    console.log(menuStatus)
+    
+    const ToggleMenuHandler = ()=> {
+        setMenuStatus(!menuStatus)
+    }
+
+    const classnamesmall = clsx({ "hamburger-menu-list": true,
+                             "hamburger-menu-open": menuStatus,
+                             "hamburger-menu-closed": !menuStatus})
+    const classname = size.width >= 750 ? 'menu' : classnamesmall
 
     return ( 
-        <>
+        <div className="main-navbar">
             <Link class="title" href="/"><a>'t Klein Moment</a></Link>
-            <div>
-                <Link class="navbar" href="/about"><a>About</a></Link>
-                <Link class="navbar" href="/albums"><a>Albums</a></Link>
-                <Link class="navbar" href="/contact"><a>Contact</a></Link>
-                { !isLoggedIn &&
-                    <Link class="navbar" href="/login"><a>Login</a></Link>
-                    ||
-                    <>
-                        <Link class="navbar" href="/profiel"><a>Profiel</a></Link>
-                        <Link class="navbar" href="/favorieten"><a>Favorieten</a></Link>
-                        <Link class="navbar" href="/logout"><a>Logout</a></Link>
-                    </>
-                }
-            </div>
-        </>
+            <nav>
+                <button className="mobile-hamburger" onClick={ToggleMenuHandler}>MENU</button>
+                <div className={classname} onClick={ToggleMenuHandler}>
+                    <ul>
+                        <li>
+                            <Link href="/about"><a>About</a></Link>
+                        </li>
+                        <li>
+                            <Link href="/albums"><a>Albums</a></Link>
+                        </li>
+                        <li>
+                            <Link href="/contact"><a>Contact</a></Link>
+                        </li>
+                        { !isLoggedIn &&
+                            <li>
+                                <Link href="/login"><a>Login</a></Link>
+                            </li>
+                            ||
+                            <li>
+                                <Link href="/favorieten"><a>Profiel</a></Link>
+                                <ul>
+                                    <li>
+                                        <Link href="/profiel"><a>Account</a></Link>
+                                    </li>
+                                    <li>
+                                        <Link href="/favorieten"><a>Favorieten</a></Link>
+                                    </li>
+                                    <li>
+                                        <Link href="/logout"><a>Logout</a></Link>
+                                    </li>
+                                </ul>
+                            </li>
+                        }
+                    </ul>
+                </div>
+            </nav>
+        </div>
     )
 }
--- components/layout.js
+++ components/layout.js
@@ -8,15 +8,18 @@ export default ({ children, footerData, title }) => {
     return (
         <div>
             <Head>
+                <meta charSet="UTF-8"/>
                 <title>{title}</title>
                 <link rel="icon" href="/favicon.ico" />
+                <meta name="description" content=""/>
+                <meta name="viewport" content="width=device-width, initial-scale=1"/>
             </Head>
             <header>
                 <Navbar />
             </header>
         
             <main>
-                    {children}
+                {children}
             </main>
 
             <footer>
