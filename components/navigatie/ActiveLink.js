c8cd6d143cf44232e9f6073697e5d0586abbd23b
Roel <roel.van.bilzen@gmail.com>
2020-06-20 16:52:12 +0200

    Set active link & change grayscale for phone and tablet users

---
+++ components/navigatie/ActiveLink.js
@@ -0,0 +1,20 @@
+import { withRouter } from 'next/router';
+import Link from 'next/link';
+import React, { Children } from 'react';
+
+// Script die de active navigatie link de class xx geeft, waarbij xx gedefinieerd word in de class zelf
+// ex: activeClassName="active"
+const ActiveLink = ({ router, children, ...props }) => {
+  const child=Children.only(children);
+
+  let className=child.props.className||'';
+  if (router.pathname===props.href&&props.activeClassName) {
+     className=`${className}${props.activeClassName}`.trim();
+  }
+
+  delete props.activeClassName;
+
+  return<Link{...props}>{React.cloneElement(child, { className })}</Link>;
+};
+
+export default withRouter(ActiveLink);
