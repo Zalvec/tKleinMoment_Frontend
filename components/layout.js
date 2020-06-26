fb556ade7d736da6c759afa5708ee2b87c4bdf20
Roel <roel.van.bilzen@gmail.com>
2020-06-26 10:19:59 +0200

    adding robots.txt and manifest.json

--- components/layout.js
+++ components/layout.js
@@ -13,6 +13,7 @@ export default ({ children, footerData, title }) => {
                     <meta charSet="UTF-8"/>
                     <title>{title}</title>
                     <link rel="icon" href="/favicon-lens.jpg" />
+                    <link rel="manifest" href="/manifest.webmanifest"></link>
                     <meta name="description" content=""/>
                     <meta name="viewport" content="width=device-width, initial-scale=1"/>
                 </Head>
